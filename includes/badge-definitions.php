e36d7b8b626a265096452b7d8dfe80f719ab993a
Andrew Downes <mrdownes@hotmail.com>
2015-02-12 17:46:19 -0600

    return badge class based on user's language

--- includes/badge-definitions.php
+++ includes/badge-definitions.php
@@ -1,10 +1,11 @@
 <?php
-//TODO: consider storing badge class data in the LRS either as a statement or in the Activity Profile API
+
+//TODO: consider allowing the prototype user to specify this data and even upload a badge image to the LRS. 
 
 $badgeDefinitions = array(
     "1" => array(
-        "name" => array("en"=>"Example Tin Badge number one"),
-        "description" => array("en"=>"The first example Tin Badge"),
+        "name" => array("en"=>"Example Tin Badge number one", "fr"=>"Exemple Tin Badge numéro un"),
+        "description" => array("en"=>"The first example Tin Badge", "fr"=>"Le premier exemple Tin Badge"),
         "type" => "http://activitystrea.ms/schema/1.0/badge",
         "extensions" => array(
             "http://standard.openbadges.org/xapi/extensions/badgeclass.json" => array(
@@ -21,5 +22,19 @@ $badgeDefinitions = array(
                 "@id" => $CFG->wwwroot . "/resources/badge-class.php?badge-id=2"
             )
         )
+    )
+);
+
+//TODO: store the badge class specifc data in the Activity Profile API
+$badgeClassData = array(
+    "1" => array(
+        "image" => $CFG->wwwroot ."/badges/badge-one.png",
+        "criteria" => $CFG->wwwroot ."/resources/criteria.php?badge-id=1",
+        "issuer" => $CFG->wwwroot ."/resources/issuer-organization.json"
+    ),
+    "2" => array(
+        "image" => $CFG->wwwroot ."/badges/badge-two.png",
+        "criteria" => $CFG->wwwroot ."/resources/criteria.php?badge-id=2",
+        "issuer" => $CFG->wwwroot ."/resources/issuer-organization.json"
     )
 );
--- resources/badge-class.php
+++ resources/badge-class.php
@@ -5,6 +5,12 @@
 Recieves querystring paramaters, returns hard coded badge class json.
 */
 include "../config.php";
+include "../includes/badges-lib.php";
+
+//TODO: get the badge definitions from the LRS rather than from $badgeDefinitions to simulate the idea that
+// the badge class.php does not need to rely on hard coded badge definitions. 
+include "../includes/badge-definitions.php";
+
 header('Content-Type: application/json');
 
 if (isset($_GET["badge-id"])){
@@ -15,31 +21,22 @@ if (isset($_GET["badge-id"])){
     die();
 }
 
-//TODO: use data from badge-definitions.php
-        //TODO: Use Accept-language to return the canonical activity definition in the requested language, thus solving localization
-        // of Badge classes in Open Badges
-
-$badges = array(
-    "1" => array(
-        "name" => "Example Tin Badge number one",
-        "description" => "The first example Tin Badge",
-        "image" => $CFG->wwwroot ."/badges/badge-one.png",
-        "criteria" => $CFG->wwwroot ."/resources/criteria.php?badge-id=1",
-        "issuer" => $CFG->wwwroot ."/resources/issuer-organization.json"
-    ),
-        "2" => array(
-        "name" => "Example Tin Badge number two",
-        "description" => "The second example Tin Badge",
-        "image" => $CFG->wwwroot ."/badges/badge-two.png",
-        "criteria" => $CFG->wwwroot ."/resources/criteria.php?badge-id=2",
-        "issuer" => $CFG->wwwroot ."/resources/issuer-organization.json"
-    )
-);
-
-if (isset($badges[$badgeId])){
-    echo json_encode($badges[$badgeId], JSON_UNESCAPED_SLASHES);
+if (isset($badgeDefinitions[$badgeId])){
+    $badgeDefinition = $badgeDefinitions[$badgeId];
+    $badgeClassDatum = $badgeClassData[$badgeId];
+    echo json_encode(
+        array(
+            "name" => getAppropriateLanguageMapValue($badgeDefinition["name"]),
+            "description" => getAppropriateLanguageMapValue($badgeDefinition["description"]),
+            "image" => $badgeClassDatum["image"],
+            "criteria" => $badgeClassDatum["criteria"],
+            "issuer" => $badgeClassDatum["issuer"]
+        ), 
+        JSON_UNESCAPED_SLASHES
+    );
 } else {
     header("HTTP/1.1 404 Not Found");
     http_response_code(404);
     die();
 }
+
